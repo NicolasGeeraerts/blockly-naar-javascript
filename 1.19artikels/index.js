a676a91d0b7d9fde2326a97f4d44d34e8a3bbf1c
nicolas geeraerts <nico199411@hotmail.be>
2023-02-08 11:16:36 +0100

    eindoefening

---
+++ 1.19artikels/index.js
@@ -0,0 +1,38 @@
+"use strict";
+const artikels = [];
+let naam = prompt("Naam");
+while (naam !== "stop") {
+    artikels.push({
+        naam: naam,
+        aanKoopPrijs: Number(prompt("aankoopprijs")),
+        verKoopPrijs: Number(prompt("verkoopprijs"))
+    });
+    naam = prompt("Naam");
+}
+const opzoekking = prompt("welk artikel zoekt u ")
+const opgezochteArtikels = artikels.filter(artikel => artikel.naam === opzoekking);
+opgezochteArtikels.forEach(artikel => console.log(artikel.naam));
+const getal1 = Number(prompt("verkoopprijs opzoeken van ?"))
+const getal2 = Number(prompt("tot"))
+if (getal1 > getal2) {
+    alert("het 2de getal moet groter zijn dan het eerste")
+}
+else {
+    const opgezochteprijs = artikels.filter(artikel => artikel.verKoopPrijs >= getal1 && artikel.verKoopPrijs <= getal2);
+    opgezochteprijs.forEach(artikel => console.log(artikel.naam, ";", artikel.verKoopPrijs))
+}
+let sum = 0;
+artikels.forEach(item => sum += item.verKoopPrijs);
+console.log("de gemiddelde verkoopprijs is : " + sum / artikels.length);
+
+
+
+
+
+
+
+
+
+
+
+
